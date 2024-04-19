dc744d3b0aaf63086af18ed7a8e637f30f26d5c2
Edenilson <edenilsonju10@gmail.com>
2024-04-19 17:30:05 -0300

    Add project files.

---
+++ PorcentSalario/Program.cs
@@ -0,0 +1,34 @@
+﻿// 5- Faça um programa que leia o valor atual do salario de uma pessoa e o
+// valor com acréscimo e informe a porcentagem de acréscimo que foi dado (conferir se o proximo valor é maior)
+double salarioAntigo = 0, salarioNovo = 0;
+double resultado = 0;
+Console.WriteLine("=====Calcular porcentagem=====");
+
+do
+{
+    Console.Write("Digite o salario antigo: ");
+    salarioAntigo = double.Parse(Console.ReadLine());
+
+    if(salarioAntigo <= 0) Console.WriteLine("Valor invalido!");
+
+} while (salarioAntigo <= 0);
+Console.WriteLine("=============================");
+do
+{
+    Console.Write("Digite o salario novo: ");   
+    salarioNovo = double.Parse(Console.ReadLine());
+
+    if (salarioNovo <= 0) Console.WriteLine("Salario novo menor que o antigo!");
+
+} while (salarioNovo < salarioAntigo);
+
+resultado = (salarioAntigo / salarioNovo) * 100;
+
+Console.WriteLine("=============================");
+Console.WriteLine($"O aumento foi de {resultado}%");
+Console.WriteLine("=============================");
+
+
+
+Console.Write("\nDigite qualquer tecla para sair: ");
+Console.ReadKey();
